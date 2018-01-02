8a8ecc6498b6a31641158b3a65a85664cc536786
captain-yuca <manuel.baez@upr.edu>
2018-01-02 09:47:14 -0400

    Added main.py routes to work with Flask. Added requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Help
+from flask import Flask, jsonify, request
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def greeting():
+    return 'Hello, this is the parts DB App!'
+
+@app.route('/api/', methods=['GET'])
+def getAllParts():
+    return '<h3>HELLO</h3>'
+
+
+if __name__ == '__main__':
+    app.run()
