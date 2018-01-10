113204fc44f580b4be4e739ac12cb0586d7c6342
captain-yuca <manuel.baez@upr.edu>
2018-01-09 23:30:01 -0400

    WORKS

--- dao/purchase.py
+++ dao/purchase.py
@@ -6,7 +6,7 @@ class PurchaseDAO:
     def __init__(self):
 
         connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'], pg_config['user'], pg_config['passwd'])
-        self.conn = psycopg2._connect(connection_url)
+        self.conn = psycopg2._connect('postgres://ebwyoylyofgguf:bce5e728f4d5504e7a83ba37ece6f608483572bb56baa5700c90dab37d151295@ec2-54-83-59-144.compute-1.amazonaws.com:5432/de320ggpcvnl39')
 
     def getAllPurchases(self):
         cursor = self.conn.cursor()
