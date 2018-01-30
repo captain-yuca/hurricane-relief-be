43da2a0cd3dd612e050e62d6f6ed0f09657571b1
captain-yuca <manuel.baez@upr.edu>
2018-01-30 19:10:45 -0400

    Added to work with aws

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -3,8 +3,8 @@ from urllib import parse
 import psycopg2
 
 parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["DATABASE_URL"])
-
+# url = parse.urlparse(os.environ["DATABASE_URL"])
+url = parse.urlparse("postgresql://appusr:appusr1@hurricanerelief.cybd6a6s6wh4.us-east-1.rds.amazonaws.com/appdb")
 ##appusr
 ##appusr1
 ##appdb
